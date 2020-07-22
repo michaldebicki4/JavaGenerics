e90d6d0548f7c606139c5f6f91e1e4339ad5c6f5
michaldebicki4 <michaldebicki4@gmail.com>
2020-07-22 14:22:19 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> items = new ArrayList<>();
+
+        items.add(1);
+        items.add(2);
+  //      items.add("tim");
+        items.add(4);
+        items.add(5);
+
+        printDoubled(items);
+
+
+
+    }
+    public static void printDoubled(ArrayList<Integer> n){
+        for(Object i : n){ //może być tu int
+            System.out.println((Integer) i * 2); //metoda (Integer) - casting
+        }
+    }
+}
